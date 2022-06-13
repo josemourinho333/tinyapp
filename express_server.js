5281b38784810261352625cd815c7604e5c0fcac
josemourinho333 <yoo.phil92@gmail.com>
2022-06-13 16:56:46 -0700

    Update with generateRandomString()

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
+
+function generateRandomString() {
+  const randomNum = () => {
+    return Math.floor(Math.random() * 26) + 97;
+  }
+
+  return String.fromCharCode(randomNum()).toUpperCase() + String.fromCharCode(randomNum()) + String.fromCharCode(randomNum()).toUpperCase() + String.fromCharCode(randomNum()) + String.fromCharCode(randomNum()).toUpperCase() + String.fromCharCode(randomNum())
+};
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDataBase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,18 +25,24 @@ app.get('/', (request, response) => {
   response.send('Hello');
 });
 
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (request, response) => {
   const templateVars = { shortURL: request.params.shortURL, longURL: request.params.longURL };
   response.render('urls_show', templateVars);
 });
-
 
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDataBase };
   response.render('urls_index', templateVars);
 });
 
-
+app.post('/urls', (request, response) => {
+  console.log(request.body);
+  response.send('ok you good');
+})
 
 app.listen(PORT, () => {
   console.log(`App now listening on port ${PORT}...`);
