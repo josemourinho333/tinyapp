244b37f599f98f16a0fbb7dadb18664dfc8bd42d
josemourinho333 <yoo.phil92@gmail.com>
2022-06-13 12:59:52 -0700

    Delete unneeded examples

--- express_server.js
+++ express_server.js
@@ -11,13 +11,6 @@ app.get('/', (request, response) => {
   response.send('Hello');
 });
 
-app.get('/urls.json', (request, response) => {
-  response.json(urlDataBase);
-});
-
-app.get('/hello', (request, response) => {
-  response.send('<html><body style="color:red"> Hello <b>bb</b> </body></html>\n')
-})
 
 app.listen(PORT, () => {
   console.log(`App now listening on port ${PORT}...`);
